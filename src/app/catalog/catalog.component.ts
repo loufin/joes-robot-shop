982f103bd577ceba3a81da8bee419d292498c1c0
loufin <louis.finney1@gmail.com>
2024-12-16 19:55:58 -0600

    module 7 complete

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-catalog',
@@ -9,9 +10,8 @@ import { IProduct } from './product.model';
 export class CatalogComponent {
   products: IProduct[];
   filter : string = '';
-  cart: IProduct[] = [];
 
-  constructor() {
+  constructor(private cartSvc: CartService) {
     this.products  = [
       {
         id: 1,
@@ -201,6 +201,6 @@ export class CatalogComponent {
   }
   
   addToCart(product: IProduct) {
-    this.cart.push(product);
+    this.cartSvc.add(product);
   }
 }
